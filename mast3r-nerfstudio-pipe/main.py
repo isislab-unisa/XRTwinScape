4229c5ae094a88f6953640213f5425628948786f
Ater_mor <48551962+AndreaAmorosini@users.noreply.github.com>
2025-05-03 21:13:01 +0200

    Refactors pipeline execution and enhances API integration. Fixes the retry mechanism for the pipeline

--- mast3r-nerfstudio-pipe/main.py
+++ mast3r-nerfstudio-pipe/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI, HTTPException
@@ -7,40 +8,45 @@ from pydantic import BaseModel
 from full_pipe import full_pipe
 from fastapi.middleware.cors import CORSMiddleware
 import time
-import minio
 import boto3
 from urllib.parse import urlparse
 import shutil
 import requests
 import threading
+import subprocess
 
 
 MINIO_EDNPOINT = "http://minio:9000"
 MINIO_ROOT_USER = "minioadmin"
 MINIO_ROOT_PASSWORD = "minioadmin123"
 AWS_STORAGE_BUCKET_NAME = "lessons-media"
-CALLBACK_ENDPOINT = "http://web:80/complete_build"
+CALLBACK_ENDPOINT = "http://web:8001/complete_build/"
+TOKEN_REQUEST_ENDPOINT = "http://web:8001/api/token/"
+
 
 class CustomHTTPException(HTTPException):
     def __init__(self, status_code: int, detail: str, error_code: int):
         super().__init__(status_code=status_code, detail=detail)
         self.error_code = error_code
-        
+
+
 class Response(BaseModel):
     ply_url: str | None = None
     message: str | None = None
+
 
 class Request(BaseModel):
     lesson_id: str | None = None
     lesson_name: str | None = None
     video_url: str | None = None
     training_type: str | None = None
-    
+    token: str | None = None
 
 
 RETRY_LIMIT = 3
 RETRY_COUNTER = 0
 RETRY_COOLDOWN = 180  # seconds
+
 
 app = FastAPI()
 
@@ -54,24 +60,27 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
+
 s3 = boto3.client(
-    's3',
+    "s3",
     endpoint_url=MINIO_EDNPOINT,
     aws_access_key_id=MINIO_ROOT_USER,
-    aws_secret_access_key=MINIO_ROOT_PASSWORD
+    aws_secret_access_key=MINIO_ROOT_PASSWORD,
 )
+
 
 def read_s3_file(file_name):
     try:
         video_key = file_name
         response = s3.get_object(Bucket=AWS_STORAGE_BUCKET_NAME, Key=video_key)
         print("RESPONSE:" + str(response))
-        data = response['Body'].read()
+        data = response["Body"].read()
         return data, video_key
     except Exception as e:
         print(f"Error reading file from S3: {e}")
         return None
-    
+
+
 def write_s3_file(file_path, remote_path):
     try:
         s3.upload_file(
@@ -82,40 +91,82 @@ def write_s3_file(file_path, remote_path):
         print(f"File {remote_path} written to S3")
     except Exception as e:
         print(f"Error writing file {file_path} to S3: {e}")
-        
-def process_full_pipe(request: Request, lesson_dir:str, video_path: str):
+
+
+def run_pipeline_subproc(
+    video_path: str,
+    output_dir: str,
+    frame_count: int,
+    max_num_iterations: int,
+    nerfstudio_model: str,
+    advanced_training: bool,
+    use_mcmc: bool,
+    num_downscales: int,
+    start_over: bool,
+):
+    for attempt in range(1, RETRY_LIMIT + 1):
+        print(f"Attempt {attempt} to run pipeline")
+        try:
+            cmd = [
+                "python",
+                "full_pipe.py",
+                "--video-path",
+                video_path,
+                "--output-dir",
+                output_dir,
+                "--frame-count",
+                str(frame_count if attempt == 1 else 400),
+                "--max-num-iterations",
+                str(max_num_iterations),
+                "--nerfstudio-model",
+                nerfstudio_model,
+                "--num-downscales",
+                str(num_downscales if attempt == 1 and num_downscales < 8 else 4),
+                "--start-over",
+                "True" if start_over else "False",
+            ]
+            if advanced_training:
+                cmd.append("--advanced-training")
+            if use_mcmc:
+                cmd.append("--use-mcmc")
+            print("Running command:", " ".join(cmd))
+
+            subprocess.run(cmd, check=True)
+            return
+        except subprocess.CalledProcessError as e:
+            print(f"Attempt {attempt} failed: {e}")
+        except Exception as e:
+            print(f"Attempt {attempt} failed: {e}")
+
+        if attempt < RETRY_LIMIT:
+            print(f"Retrying in {RETRY_COOLDOWN} seconds...")
+            time.sleep(RETRY_COOLDOWN)
+        else:
+            raise RuntimeError("Max attempts reached. Exiting.")
+
+
+def process_full_pipe(request: Request, lesson_dir: str, video_path: str):
     output_dir = f"{lesson_dir}/images"
     frame_count = 600
-    max_num_iterations = 100000
-    nerfstudio_model = "splatfacto-big" if request.training_type == "full" else "splatfacto"
+    max_num_iterations = 100000 if request.training_type == "full" else 30000
+    nerfstudio_model = (
+        "splatfacto-big" if request.training_type == "full" else "splatfacto"
+    )
     num_downscales = 2 if request.training_type == "full" else 8
-    
+
     try:
-        #RUN THE FULL PIPELINE            
-        for attempt in range(1, RETRY_LIMIT + 1):
-            try:
-                full_pipe(
-                    video_path=video_path,
-                    frame_output_dir=output_dir,
-                    frame_count=frame_count if attempt == 1 else 400,
-                    max_num_iterations=max_num_iterations,
-                    nerfstudio_model=nerfstudio_model,
-                    advanced_training = True if request.training_type == "full" else False,
-                    use_mcmc = True if request.training_type == "full" else False,
-                    num_downscales=num_downscales if attempt == 1 else 4,
-                    #TODO DA RIMODIFICARE IN TRUE
-                    start_over=True,
-                )
-                print("Pipeline completed successfully.")
-                break  # Exit the loop if successful
-            except Exception as e:
-                print(f"Attempt {attempt} failed: {e}")
-                if attempt <= RETRY_LIMIT:
-                    print(f"Retrying in {RETRY_COOLDOWN} seconds...")
-                    time.sleep(RETRY_COOLDOWN)
-                else:
-                    print("Max attempts reached. Exiting.")
-                    raise e
+        # RUN THE FULL PIPELINE
+        run_pipeline_subproc(
+            video_path=video_path,
+            output_dir=output_dir,
+            frame_count=frame_count,
+            max_num_iterations=max_num_iterations,
+            nerfstudio_model=nerfstudio_model,
+            advanced_training=True if request.training_type == "full" else False,
+            use_mcmc=True if request.training_type == "full" else False,
+            num_downscales=num_downscales,
+            start_over=True,
+        )
 
         splat_path = (
             f"/lessons/{request.lesson_name}_{request.lesson_id}/splat/splat.ply"
@@ -129,36 +180,79 @@ def process_full_pipe(request: Request, lesson_dir:str, video_path: str):
         # DELETE FOLDER
         shutil.rmtree(lesson_dir, ignore_errors=True)
         print("Folder deleted")
-        
+
+        # print("Running full pipeline...")
+        # time.sleep(5)  # Simulate processing time
+        # # Simulate successful completion of the pipeline
+
+        # REQUEST TOKEN
+        token_payload = {
+            "username": "root",
+            "password": "root",
+        }
+
+        token_response = requests.post(
+            TOKEN_REQUEST_ENDPOINT,
+            json=token_payload,
+        )
+        print(
+            "Token response:",
+            token_response.status_code,
+            token_response.text,
+            flush=True,
+        )
+        token_access = token_response.json().get("access")
+
         callback_payload = {
             "lesson_id": request.lesson_id,
             "lesson_name": request.lesson_name,
             "ply_path": f"{request.lesson_name}_{request.lesson_id}/splat.ply",
-            "status": "completed",
+            "status": "COMPLETED",
         }
-        
+
+        headers = {
+            "Authorization": f"Bearer {token_access}",
+        }
+
         try:
             response = requests.post(
                 CALLBACK_ENDPOINT,
                 json=callback_payload,
+                headers=headers,
             )
-            print("Callback response:", response.status_code, response.text)
+            print("Callback response:", response.status_code, response.text, flush=True)
         except requests.RequestException as e:
             print(f"Error sending callback: {e}")
-            
+
     except Exception as e:
-        print(f"Error processing full pipeline: {e}")
+        print(f"Error processing full pipeline: {e}", flush=True)
         callback_payload = {
             "lesson_id": request.lesson_id,
             "lesson_name": request.lesson_name,
             "ply_path": None,
-            "status": "failed",
+            "status": "FAILED",
         }
-        
+
+        token_payload = {
+            "username": "root",
+            "password": "root",
+        }
+
         try:
+            token_response = requests.post(
+                TOKEN_REQUEST_ENDPOINT,
+                json=token_payload,
+            )
+            print("Token response:", token_response.status_code, token_response.text)
+            token_access = token_response.json().get("access")
+            headers = {
+                "Authorization": f"Bearer {token_access}",
+            }
+
             response = requests.post(
                 CALLBACK_ENDPOINT,
                 json=callback_payload,
+                headers=headers,
             )
             print("Callback response:", response.status_code, response.text)
         except requests.RequestException as e:
@@ -173,48 +267,43 @@ async def read_root():
 @app.post("/extract_ply")
 async def extract_ply(request: Request) -> Response:
     try:
-        #CREATE A DIRECTORY FOR THE LESSON
+        print(f"REQUEST: {request}")
+        # CREATE A DIRECTORY FOR THE LESSON
         lesson_dir = f"/lessons/{request.lesson_name}_{request.lesson_id}"
-        os.makedirs(lesson_dir, exist_ok=True)        
-        #RETRIEVE THE VIDEO FROM MINIO
+        os.makedirs(lesson_dir, exist_ok=True)
+        # RETRIEVE THE VIDEO FROM MINIO
         video, video_key = read_s3_file(request.video_url)
         if not video:
             raise CustomHTTPException(
-                status_code=404,
-                detail="Video not found",
-                error_code=1000
+                status_code=404, detail="Video not found", error_code=1000
             )
-            
-        print("VIDEO FOUND")        
-        #SAVE THE VIDEO TO THE LESSON DIRECTORY
+
+        print("VIDEO FOUND")
+        # SAVE THE VIDEO TO THE LESSON DIRECTORY
         video_path = f"{lesson_dir}/{video_key.split('/')[-1]}"
         with open(video_path, "wb") as video_file:
             video_file.write(video)
-            
+
         worker_thread = threading.Thread(
             target=process_full_pipe,
             args=(request, lesson_dir, video_path),
             daemon=True,
         )
         worker_thread.start()
-        
-        time.sleep(60)
+
+        # return Response(
+        #     message="Processing started. You will be notified once it is completed."
+        # )
+
+        # time.sleep(20)
         if worker_thread.is_alive():
             return Response(
                 message="Processing started. You will be notified once it is completed."
             )
         else:
             raise CustomHTTPException(
-                status_code=500,
-                detail="Processing failed",
-                error_code=1002
+                status_code=500, detail="Processing failed", error_code=1002
             )
-    
+
     except Exception as e:
-        raise CustomHTTPException(
-            status_code=500,
-            detail=str(e),
-            error_code=1001
-        )
-        
-        
+        raise CustomHTTPException(status_code=500, detail=str(e), error_code=1001)
