74618779e1b87e13aefca0690d48267e9ac3f43d
DarioDeMaio <demaio.dario95@gmail.com>
2025-07-02 11:18:13 +0200

    feat: refresh_cert.sh

--- twin_scape/refresh_cert.sh
+++ twin_scape/refresh_cert.sh
@@ -3,3 +3,4 @@ cd /home/xrtwinscape/XRTwinScape/twin_scape
 /usr/bin/docker-compose run --rm certbot-renew
 
 #### chmod +x /home/xrtwinscape/XRTwinScape/twin_scape/certbot_renew.sh
+#### crontab -e
