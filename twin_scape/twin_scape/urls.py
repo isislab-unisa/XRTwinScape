c1db74e87ca5ee81e6bd87a187ebbdd762b4d3be
DarioDeMaio <demaio.dario95@gmail.com>
2025-07-02 17:15:21 +0200

    feat: landin_page

--- twin_scape/twin_scape/urls.py
+++ twin_scape/twin_scape/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 # from .views import DashboardView
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', home, name="home"),
+    path('admin/', admin.site.urls),
     path('', include('twin_scape_core.urls')),
 ]
