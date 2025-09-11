5da651ec9a33a28641c179656a24e24e39ffcb29
DarioDeMaio <demaio.dario95@gmail.com>
2025-09-11 17:29:41 +0200

    fix: get_lessons

--- twin_scape/twin_scape_core/serializers.py
+++ twin_scape/twin_scape_core/serializers.py
@@ -2,6 +2,15 @@ from .models import Lesson
 from rest_framework import serializers
 
 class LessonSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+    video_file = serializers.SerializerMethodField()
+
     class Meta:
-        model=Lesson
-        fields = '__all__'
+        model = Lesson
+        exclude = ("splat_ply", "annotation_ply")
+
+    def get_images(self, obj):
+        return obj.images.name if obj.images else None
+
+    def get_video_file(self, obj):
+        return obj.video_file.name if obj.video_file else None
