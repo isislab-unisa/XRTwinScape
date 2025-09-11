c4ccc7d30336bd2675e693ce5cbffcfffdfa243b
DarioDeMaio <demaio.dario95@gmail.com>
2025-09-11 17:22:18 +0200

    fix: nginx.conf

---
+++ twin_scape/twin_scape_core/serializers.py
@@ -0,0 +1,7 @@
+from .models import Lesson
+from rest_framework import serializers
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Lesson
+        fields = '__all__'
